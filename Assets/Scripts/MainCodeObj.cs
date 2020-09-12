aa6d9c6665419e726f1bb323370181372e6867d2
Linus Jarbo <linus@linusjarbo.com>
2020-09-12 21:26:06 +0200

    Working version

--- Assets/Scripts/MainCodeObj.cs
+++ Assets/Scripts/MainCodeObj.cs
@@ -11,10 +11,6 @@ public class MainCodeObj : MonoBehaviour
     public InputField textOutput;
     public string inputText;
     public Toggle toggle;
-    public Toggle toggle_removeLastDigit;
-    public Toggle toggle_addFirstDidgit;
-    public Toggle toggle_recursive;
-    public InputField recursive_steps;
     public string currentIdentity = "not set yet";
     public GameObject infoscreen;
 
@@ -35,180 +31,78 @@ public class MainCodeObj : MonoBehaviour
 
         clearOutputText();
 
+        //emtpy list if items added previously
+        inputList.Clear();
         //add input to List
         for (int i = 0; i < lines.Length; i++)
         {
             inputList.Add(int.Parse(lines[i]));
         }
 
-        List<int> tempInputList = new List<int>();
-        tempInputList = inputList;
-
+        List<int> tempInputList = new List<int>(inputList);
         List<int> idNumber_list = new List<int>();
-        for (int i = 0; i < inputList.Count; i++)
+
+        //input number is member of first row
+        idNumber_list.Add(inputList[1]);
+        printOutListToOutput(inputList);
+
+        for (int i = 0; i < inputList.Count-1; i++)
         {
             idNumber_list.Add(tempInputList[1]);
             //recursive
             tempInputList = FoldListSequence(tempInputList);
+
+            printOutListToOutput(tempInputList);
         }
-
-        /*
-                for (int i = 0; i < inputList.Count; i++)
-                {
-                    Debug.Log(inputList[i].ToString());
-                }
-        */
-
-        /*
-        //cut up text in linebreaks
-        string[] lines = inputText.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
-
-        //text ok, clear output
-        clearOutputText();
-
-        //if lines are greater than one and recursive method, return
-        if (toggle_recursive.isOn && lines.Length > 1)
-        {
-            addTextToOutput("Recursive method only works on single line of input");
-            return;
-        }
-        else if (toggle_recursive.isOn)
-        {
-            //integers from inputfield
-            int recursive_int = int.Parse(recursive_steps.text);
-
-            for (int z = 0; z < recursive_int; z++)
-            {
-                if (z == 0)
-                {
-                    GetFoldIdentity(lines);
-                }
-                else
-                {
-                    string[] currID = currentIdentity.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
-                    GetFoldIdentity(currID);
-                }
-            }
-        }
-        else
-        {
-            GetFoldIdentity(lines);
-        }
-        */
     }
 
+    public void printOutListToOutput(List<int> pList)
+    {
+        string pString = "";
+        for (int i = 0; i < pList.Count; i++)
+        {
+            if (i != pList.Count - 1)
+            {
+                pString += pList[i].ToString() + ",";
+            }
+            else
+            {
+                pString += pList[i].ToString();
+            }
+        }
+        addTextToOutput(pString + "\n");
+    }
 
     private List<int> FoldListSequence(List<int> seqList)
     {
-        List<int> tempList = new List<int>();
-        tempList = seqList;
+        List<int> tempList = new List<int>(seqList);
 
         //fold templist
-        List<int> firstHalf = tempList.GetRange(0, tempList.Count / 2);
-        List<int> secondHalf = tempList.GetRange(tempList.Count / 2, tempList.Count);
+        List<int> firstHalf = tempList.GetRange(0, (tempList.Count / 2));
+        List<int> secondHalf = tempList.GetRange((tempList.Count / 2), tempList.Count / 2);
 
         //reverse second Half
         secondHalf.Reverse();
 
         List<int> returnList = new List<int>();
 
+        int countfirst = 0;
+        int countsecond = 0;
         for (int i = 0; i < seqList.Count; i++)
         {
             if (i % 2 == 0)
             {
-                returnList.Add(secondHalf[i]);
+                returnList.Add(firstHalf[0]);
+                firstHalf.RemoveAt(0);
             }
             else
             {
-                returnList.Add(secondHalf[i]);
+                returnList.Add(secondHalf[0]);
+                secondHalf.RemoveAt(0);
             }
         }
-
         return returnList;
     }
-    /*
-        private void GetFoldIdentity(string[] lines)
-        {
-
-            for (int j = 0; j < lines.Length; j++)
-            {
-
-                //cut up string in two halfs and fold
-                string s = lines[j];
-                char firstDigit = s[0];
-                //Debug.Log(firstDigit);
-                //validate string
-                s = validateText(s);
-
-                string foldedIdentity = "";
-                int strLength = s.Length; //need length twice
-                for (int i = 0; i < strLength; i++)
-                {
-                    if (i == 0)
-                    {
-                        addTextToOutput("\n" + "Input: " + s);
-                    }
-
-
-                    if (toggle.isOn)
-                    {
-                        addTextToOutput(s);
-                    }
-
-                    if (strLength >= 0)
-                    {
-                        foldedIdentity += s.ToString()[1];
-                    }
-
-                    s = FoldSequence(s, strLength);
-                }
-
-                if (toggle_removeLastDigit.isOn)
-                {
-                    foldedIdentity = foldedIdentity.Remove(foldedIdentity.Length - 1);
-                }
-
-                if (toggle_addFirstDidgit.isOn)
-                {
-                    foldedIdentity = firstDigit + foldedIdentity;
-                }
-
-                addTextToOutput("Fold : " + foldedIdentity);
-                currentIdentity = foldedIdentity;
-            }
-        }
-        */
-
-
-    /*
-    private string FoldSequence(string s, int l)
-    {
-        //string length
-        int strLength = l;
-
-        //mixed string
-        string mixedString = "";
-        char[] mixedCharArray = new char[strLength];
-
-        //take string and divide in two substrings
-        string firstPart = s.Substring(0, strLength / 2);
-        string secondPart = s.Substring(strLength / 2, strLength / 2);
-
-        //reverse second part
-        secondPart = Reverse(secondPart);
-
-        char[] firstPartCharArr = firstPart.ToCharArray();
-        char[] secondPartCharArr = secondPart.ToCharArray();
-
-        //Mix arrays into new string
-        for (int i = 0; i < strLength / 2; i++)
-        {
-            mixedString += firstPartCharArr[i];
-            mixedString += secondPartCharArr[i];
-        }
-        return mixedString;
-    }
-    */
 
     public void copyToMemory()
     {
@@ -217,31 +111,8 @@ public class MainCodeObj : MonoBehaviour
 
     private string GetInputFieldText()
     {
-        //Debug.Log(textInput.text);
         return (textInput.text);
     }
-
-    /*
-        private string validateText(string s)
-        {
-            if (s.Length % 2 == 0)
-            {
-                //do nothing
-            }
-            else if (s.Length == 0)
-            {
-                //string is empty do something...
-                s = "::";
-            }
-            else
-            {
-                addTextToOutput("Size not even, trimming end from: " + s);
-                s = s.Remove(s.Length - 1);
-                addTextToOutput("->                            to: " + s);
-            }
-            return (s);
-        }
-    */
 
     private void addTextToOutput(string s)
     {
@@ -252,20 +123,4 @@ public class MainCodeObj : MonoBehaviour
     {
         textOutput.text = "";
     }
-
-    /*
-        public static string Reverse(string s)
-        {
-            char[] charArray = s.ToCharArray();
-            Array.Reverse(charArray);
-            return new string(charArray);
-        }
-    */
-
-    /*
-        public void toggleInfoScreen(bool isVisible)
-        {
-            //infoscreen.SetActive(isVisible);
-        }
-    */
 }
